135822654c8e6e317268b0a615603da3054dfe00
UpSetSummerSet <72717739+UpSetSummerSet@users.noreply.github.com>
2020-10-28 23:50:16 +0800

    Done2

--- CodeMini_16TanKeeLiang/Assets/Scripts/PlayerController15.cs
+++ CodeMini_16TanKeeLiang/Assets/Scripts/PlayerController15.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 public class PlayerController15 : MonoBehaviour
 {
     float speed = 10.0f;
-    float zAxis = 10.0f;
+    float planeAabsLimits = 10.0f;
+    float planeBabsLimits = 5.0f;
+
+    float relzLimit;
+    float relxLimit;
+
     int planeIndicator = 0;
-    float xAxis = 10.0f;
+
+    float gravityModifier = 2.0f;
+    float jumpForce = 8.0f;
+    int spaceTrack = 0;
+
+    Rigidbody playerRb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
@@ -24,39 +38,105 @@ public class PlayerController15 : MonoBehaviour
        
         if(planeIndicator == 0)
         {
+            relzLimit = planeAabsLimits;
+            relxLimit = planeAabsLimits;
 
-            if (transform.position.z < -zAxis)
+            if (transform.position.z > relzLimit)
             {
-                if (transform.position.x > xAxis / 2 && transform.position.x < xAxis)
+                if (transform.position.x > -relxLimit / 2 && transform.position.x < relxLimit / 2)
                 {
                     transform.Translate(Vector3.forward * Inputvertical * speed * Time.deltaTime);
                 }
                 else
                 {
-                    transform.position = new Vector3(transform.position.x, transform.position.y, zAxis);
+                    transform.position = new Vector3(transform.position.x, transform.position.y, relzLimit);
                 }
             }
-            else if (transform.position.z > zAxis)
+            else if (transform.position.z < -relzLimit)
             {
-                transform.position = new Vector3(transform.position.x, transform.position.y, zAxis);
+                transform.position = new Vector3(transform.position.x, transform.position.y, -relzLimit);
+            }
+            else
+            {
+                transform.Translate(Vector3.forward * Inputvertical * speed * Time.deltaTime);
+            }
+
+            if (transform.position.x > relxLimit)
+            {
+                transform.position = new Vector3(relxLimit, transform.position.y, transform.position.z);
+            }
+            else if (transform.position.x < -relxLimit)
+            {
+                transform.position = new Vector3(-relxLimit, transform.position.y, transform.position.z);
+            }
+            else
+            {
+                transform.Translate(Vector3.right * Inputhorizontal * speed * Time.deltaTime);
             }
         }
-        if (transform.position.x < -xAxis)
+        else
         {
-            transform.position = new Vector3(-xAxis, transform.position.y, transform.position.z);
+            relzLimit = planeAabsLimits + 2 * planeBabsLimits;
+            relxLimit = planeBabsLimits;
+
+            if(transform.position.z < planeAabsLimits)
+            {
+                if(transform.position.x > -planeAabsLimits / 2 && transform.position.x < planeAabsLimits / 2)
+                {
+                    transform.Translate(Vector3.forward * Inputvertical * speed * Time.deltaTime);
+                }
+                else
+                {
+                    transform.position = new Vector3(transform.position.x, transform.position.y, planeAabsLimits);
+                }
+            }
+            else if (transform.position.z > relzLimit)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, relzLimit);
+            }
+            else
+            {
+                transform.Translate(Vector3.forward * Inputvertical * speed * Time.deltaTime);
+            }
+            if (transform.position.x > relxLimit)
+            {
+                transform.position = new Vector3(relxLimit, transform.position.y, transform.position.z);
+            }
+            else if (transform.position.x < -relxLimit)
+            {
+                transform.position = new Vector3(-relxLimit, transform.position.y, transform.position.z);
+            }
+            else
+            {
+                transform.Translate(Vector3.right * Inputhorizontal * speed * Time.deltaTime);
+            }
         }
-        else if (transform.position.x > xAxis)
-        {
-            transform.position = new Vector3(xAxis, transform.position.y, transform.position.z);
-        }
+        JumpPlayer();
     }
-    /*
+    
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("PlaneA"))
         {
-            
+            Debug.Log("In Plane A");
+            planeIndicator = 0;
+            spaceTrack = 0;
+        }
+        if(collision.gameObject.CompareTag("PlaneB"))
+        {
+            Debug.Log("In Plane B");
+            planeIndicator = 1;
+            spaceTrack = 0;
         }
     }
-    */
+
+    private void JumpPlayer()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && spaceTrack < 1)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+            spaceTrack++;
+        }
+    }
 }
